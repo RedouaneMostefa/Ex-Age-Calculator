8553ca2dbcbb332917b80c1017f093135ac8d984
Redouane Mostefa <red1.roi48@gmail.com>
2018-06-16 18:40:38 +0100

    final work of Age Calculator
    i just try many methods for date and convert from string to integer and calcul Age with Years and Months And days i can make it more datails like your life in hours or minutes and seconds and milisec

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 /*jslint plusplus : true */
+/*jshint es5: false */
 /*global console, alert, prompt*/
 
 function newClacul() {
     "use strict";
-    var
-        year,
-        ybd = document.getElementById("ybd").value,
-        mbd = document.getElementById("mbd").value,
-        dbd = document.getElementById("dbd").value,
+    var cbd = document.getElementById("date").value,// required a value from type date
+        ybd = parseInt(cbd.substring(0, 4), 10),//convert a value from string to integer (year_val)
+        mbd = parseInt(cbd.substring(5, 7), 10),//convert a value from string to integer (month_val)
+        dbd = parseInt(cbd.substring(8, 10), 10),//convert a value from string to integer (day_val)
+        bd = new Date(ybd, mbd - 1, dbd),//convert our int_val to UTC Date !(mbd-1) month beg from 0
+        bdOnMls = new Date() - bd,//Subtraction our birthday from UTC date to become mellisec num
+        yOfBd = Math.floor(bdOnMls / 31536000000),//year of birthday on natural number 
+        dOfBd = Math.floor((bdOnMls % 31536000000) / 86400000),//day of birthday on natural number 
+        mOfBd = Math.floor(dOfBd / 30);//month of birthday on natural number 
     
-        ycd = document.getElementById("ycd").value,
-        mcd = document.getElementById("mcd").value,
-        dcd = document.getElementById("dcd").value;
+    dOfBd = dOfBd % 30;//rest days from months 
     
+        
+        
+        
+            
+    if (isNaN(yOfBd) || isNaN(mOfBd) || isNaN(dOfBd)) {//if a user dont put a birthday correctly
+        document.getElementById("showTime").innerHTML = "<h1>You Don't Put your Birth day date correctly</h1>";
+    } else {
     
-    year = ycd - ybd;
-    
-    
-    
-    alert(year);
-    
-    
+        document.getElementById("showTime").innerHTML =
+            "<h1> You have </h1>    "
+            +
+            " <h3 style='color:red;font-size: 30px'> " + yOfBd + " </h3> "
+            +
+            "  <h3> Year Old  And </h3> <br> "
+            +
+            "  <h3 style='color:red;font-size: 30px'>  " + mOfBd + "  </h3>  "
+            +
+            " <h3> Months And </h3> <br> "
+            +
+            "<h3 style='color:red;font-size: 30px' >  " + dOfBd + "  </h3>"
+            +
+            "  <h3>  Days   </h3>";
+    }
 }
-newClacul();
+
